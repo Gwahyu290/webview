3aace5a981d291a00b9df9f25aef4c7f53fa292c
Gwahyu290 <gistaw420@gmail.com>
2021-07-18 14:20:14 +0700

    android v1.0

--- app/src/main/java/Wahyu/gista/samchick/MainActivity.java
+++ app/src/main/java/Wahyu/gista/samchick/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity{
     private static final int INPUT_FILE_REQUEST_CODE = 1;
     private static final int FILECHOOSER_RESULTCODE = 1;
     private static final String TAG = MainActivity.class.getSimpleName();
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
